2ec0147e480d1b01283edb9ec62cad7c9698e84f
badde57 <badde57@protonmail.com>
2025-01-18 16:03:56 +0700

    test: update root integration test package imports

---
+++ tests/01-live/__init__.py
@@ -0,0 +1,6 @@
+"""Integration tests for the Chronicler project."""
+
+from . import pipeline  # noqa: F401
+from . import processors  # noqa: F401
+from . import storage  # noqa: F401
+from . import transports  # noqa: F401
